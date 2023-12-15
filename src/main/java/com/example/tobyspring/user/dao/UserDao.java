ada8a985cec76fef5526b21c7f212e2afa95b7b9
jh.kang <jh.kang@emro.co.kr>
2023-12-15 16:33:59 +0900

    1일차 마무리

--- src/main/java/com/example/tobyspring/user/dao/UserDao.java
+++ src/main/java/com/example/tobyspring/user/dao/UserDao.java
@@ -6,9 +6,16 @@ import java.sql.*;
 
 public class UserDao {
 
+    private final SimpleConnectionMaker simpleConnectionMaker;
+
+    public UserDao(ConnectionMaker connectionMaker){
+       simpleConnectionMaker = new SimpleConnectionMaker();
+    }
+
+
+
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://host.docker.internal:3306/test?useSSL=false", "root", "123456");
+        Connection c = simpleConnectionMaker.makeConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -22,8 +29,8 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://host.docker.internal:3306/test?useSSL=false", "root", "123456");
+
+        Connection c = simpleConnectionMaker.makeConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1,id);
@@ -41,24 +48,6 @@ public class UserDao {
         return user;
     }
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException{
-        UserDao dao = new UserDao();
-/*
-        User user = new User();
 
-        user.setId("Kang");
-        user.setName("강준희");
-        user.setPassword("married");
 
-        dao.add(user);
-
-        System.out.println(user.getId() + "등록 성공");*/
-
-        User user2 = dao.get("Kang");
-        System.out.println("user.getName() = " + user2.getName());
-        System.out.println("user2.getPassword() = " + user2.getPassword());
-
-        System.out.println("user2.getId() = " + user2.getId()+" 조회성공");
-
-    }
 }
